6efb064e32a54d9f3f4cf01157f958a61bf2bf7c
Meirambekov Nursultan <meirambekov.n2005@gmail.com>
2026-02-06 14:13:23 +0500

    Initial Commit

---
+++ Assignment7/Assignment7App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment7App.swift
+//  Assignment7
+//
+//  Created by pr1nce on 06.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct Assignment7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment7/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Assignment7
+//
+//  Created by pr1nce on 06.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
